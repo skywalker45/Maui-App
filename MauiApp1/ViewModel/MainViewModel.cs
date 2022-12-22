2fe44bbc467e14cd63603ea55b53d3a51f982106
skywalker45 <danielskywalker45@yahoo.com>
2022-12-22 01:24:00 -0600

    Add project files.

---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        items.Add(Text);
+
+        //Add item
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if(Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+}
