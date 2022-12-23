eb0bed9b3a0f0cacddbe6d6b99e3b75dec404f34
skywalker45 <danielskywalker45@yahoo.com>
2022-12-22 22:05:54 -0600

    Added a new page and navigation

--- MauiApp1/ViewModel/MainViewModel.cs
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -43,4 +43,10 @@ public partial class MainViewModel : ObservableObject
             Items.Remove(s);
         }
     }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
 }
